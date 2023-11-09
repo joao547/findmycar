f9a44034c96314b15bad06d0ca1a1d253d91b535
acferlucas <acferlucas.dev@gmail.com>
2023-11-09 13:07:46 -0300

    refactor

--- frontend/src/pages/home/components/HomeHeader/HomeHeader.tsx
+++ frontend/src/pages/home/components/HomeHeader/HomeHeader.tsx
@@ -1,3 +1,28 @@
-export function HomeHeader() {
-  return <h1>home header</h1>;
+import { Bell, EnvelopeSimple } from '@phosphor-icons/react';
+
+type HomeHeaderProps = {
+  currentRouteName: string;
+};
+export function HomeHeader({ currentRouteName }: HomeHeaderProps) {
+  return (
+    <header className='h-ful flex items-center p-4 justify-between'>
+      <h1 className='font-semibold text-2xl'>{currentRouteName}</h1>
+      <div className='flex items-center gap-8'>
+        <button>
+          <EnvelopeSimple size={32} />
+        </button>
+        <button>
+          <Bell size={32} />
+        </button>
+
+        <div className='w-16 h-16 inline-block rounded-full overflow-hidden'>
+          <img
+            src='https://github.com/IgorRS-2302.png'
+            alt=''
+            className='w-full h-full object-cover rounded-full'
+          />
+        </div>
+      </div>
+    </header>
+  );
 }
--- frontend/src/pages/home/components/HomeLayout/HomeLayout.tsx
+++ frontend/src/pages/home/components/HomeLayout/HomeLayout.tsx
@@ -1,5 +1,6 @@
 import { PropsWithChildren } from 'react';
 import { HomeAside } from '../HomeAside/HomeAside';
+import { HomeHeader } from '../HomeHeader/HomeHeader';
 import { Bell, EnvelopeSimple } from '@phosphor-icons/react';
 import { useLocation } from 'react-router-dom';
 
@@ -11,25 +12,7 @@ export function HomeLayout({ children }: PropsWithChildren) {
     <div className='w-full h-full flex'>
       <HomeAside />
       <div className='w-full grid grid-rows-[100px,1fr] bg-gray-200'>
-        <header className='h-ful flex items-center p-4 justify-between'>
-          <h1 className='font-semibold text-2xl'>{currentRouteName}</h1>
-          <div className='flex items-center gap-8'>
-            <button>
-              <EnvelopeSimple size={32} />
-            </button>
-            <button>
-              <Bell size={32} />
-            </button>
-
-            <div className='w-16 h-16 inline-block rounded-full overflow-hidden'>
-              <img
-                src='https://github.com/IgorRS-2302.png'
-                alt=''
-                className='w-full h-full object-cover rounded-full'
-              />
-            </div>
-          </div>
-        </header>
+        <HomeHeader currentRouteName={currentRouteName} />
         <main className='p-4'>{children}</main>
       </div>
     </div>
