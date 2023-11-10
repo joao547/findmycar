155f6768abada52820f7f038d80abe46541941b8
Rennan Prysthon <rennan.prysthon@liferay.com>
2023-11-10 14:38:59 -0300

    feat(frontend): update profile page design

--- frontend/src/pages/Profile/components/FormGroup.tsx
+++ frontend/src/pages/Profile/components/FormGroup.tsx
@@ -3,6 +3,6 @@ import { PropsWithChildren } from "react"
 export const FormGroup = ({children}: PropsWithChildren) => {
 
 	return (
-		<div className="flex flex-wrap -mx-3 mb-12">{children}</div>
+		<div className="flex flex-wrap mb-12">{children}</div>
 	)
 }
