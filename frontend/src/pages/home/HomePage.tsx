3c079e704dcdd46526fc18962e130e35ea7a9c6e
acferlucas <acferlucas.dev@gmail.com>
2023-12-04 17:06:41 -0300

    finish filter

--- frontend/src/pages/home/HomePage.tsx
+++ frontend/src/pages/home/HomePage.tsx
@@ -1,10 +1,49 @@
-/* eslint-disable max-len */
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { MagnifyingGlass, MapPin } from '@phosphor-icons/react';
-import Select, { SingleValue } from 'react-select';
+import Select from 'react-select';
 import { optionsBuscador, ufOptions } from '../../helpers/selectOptions';
 import ToggleSwitch from './components/ToggleSwitch';
+import { useRef, useState } from 'react';
+
+interface Option {
+  label: string;
+  value: string;
+}
 
 export function HomePage() {
+  const [isChecked, setIsChecked] = useState(false);
+  const ufSelectRef = useRef<any>(null);
+  const typeSelect = useRef<any>(null);
+
+  function handleToggle() {
+    setIsChecked(!isChecked);
+  }
+
+  function handlerClearFilter() {
+    setIsChecked(false);
+
+    if (ufSelectRef.current) {
+      ufSelectRef.current.clearValue();
+    }
+
+    if (typeSelect.current) {
+      typeSelect.current.clearValue();
+    }
+  }
+
+  function handlerSearch() {
+    const searchStates = ufSelectRef.current
+      .getValue()
+      .map((opt: Option) => opt.value);
+
+    const consultantType = typeSelect.current
+      .getValue()
+      .map((opt: Option) => opt.value);
+
+    console.log({
+      query: { isChecked, state: searchStates, type: consultantType },
+    });
+  }
   return (
     <div>
       <section className='flex items-center justify-center'>
@@ -21,25 +60,39 @@ export function HomePage() {
           <div className='flex gap-4 w-full'>
             <MapPin size={32} />
             <div className='w-full'>
-              <Select options={ufOptions} placeholder='Local de atuação' />
+              <Select
+                id='one'
+                options={ufOptions}
+                placeholder='Local de atuação'
+                ref={ufSelectRef}
+              />
             </div>
           </div>
 
-          <button className='bg-orange-500 h-full w-40 rounded-md'>
+          <button
+            className='bg-orange-500 h-full w-40 rounded-md text-white'
+            onClick={handlerSearch}
+          >
             Buscar
           </button>
         </div>
       </section>
       <section className='mt-4 flex items-center gap-6'>
-        <ToggleSwitch />
+        <ToggleSwitch isChecked={isChecked}>
+          <input type='checkbox' className='hidden' onChange={handleToggle} />
+        </ToggleSwitch>
         <div className=''>
           <Select
+            id='two'
             options={optionsBuscador}
             placeholder='Tipo de consultoria'
+            ref={typeSelect}
             isMulti
           />
         </div>
-        <button className='text-sm text-gray-500'>Limpar Filtros</button>
+        <button className='text-sm text-gray-500' onClick={handlerClearFilter}>
+          Limpar Filtros
+        </button>
       </section>
     </div>
   );
--- frontend/src/pages/home/components/ToggleSwitch/index.tsx
+++ frontend/src/pages/home/components/ToggleSwitch/index.tsx
@@ -1,14 +1,12 @@
-/* eslint-disable max-len */
-import React, { useState } from 'react';
+import { Children, useState, PropsWithChildren } from 'react';
 
-const ToggleSwitch = () => {
-  const [isChecked, setIsChecked] = useState(false);
-
-  const handleToggle = () => {
-    console.log(isChecked);
-    setIsChecked(!isChecked);
-  };
-
+type ToggleSwitchProps = {
+  isChecked: boolean;
+};
+export function ToggleSwitch({
+  isChecked,
+  children,
+}: PropsWithChildren<ToggleSwitchProps>) {
   return (
     <label className='flex items-center cursor-pointer gap-4'>
       <div className='text-gray-700 text-sm'>Consultor Mecanico</div>
@@ -17,20 +15,16 @@ const ToggleSwitch = () => {
           isChecked ? 'bg-orange-500' : 'bg-gray-300'
         } rounded-full shadow-inner w-14 h-7 flex items-center p-1 transition-background`}
       >
-        <input
-          type='checkbox'
-          className='hidden'
-          checked={isChecked}
-          onChange={handleToggle}
-        />
+        {children}
         <div
-          className={`toggle-dot w-6 h-6 bg-white rounded-full shadow-md transform transition-transform duration-300 ${
-            isChecked ? 'translate-x-full bg-orange-500' : ''
-          }`}
+          className={`toggle-dot w-6 h-6 bg-white rounded-full 
+            shadow-md transform transition-transform duration-300 ${
+              isChecked ? 'translate-x-full bg-orange-500' : ''
+            }`}
         ></div>
       </div>
     </label>
   );
-};
+}
 
 export default ToggleSwitch;
