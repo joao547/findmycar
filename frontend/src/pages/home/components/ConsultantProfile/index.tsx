101bc59c0c68c5650c873b56dd7f0711d4d8355c
acferlucas <acferlucas.dev@gmail.com>
2023-12-21 16:10:53 -0300

    create home layout

---
+++ frontend/src/pages/home/components/ConsultantProfile/index.tsx
@@ -0,0 +1,39 @@
+import ConsultantRate from '../ConsultantRate';
+
+type ConsultantProfileProps = {
+  consultantName: string;
+  consultantCities: string;
+  consultantArea: string;
+};
+
+export default function ConsultantProfile({
+  consultantArea,
+  consultantCities,
+  consultantName,
+}: ConsultantProfileProps) {
+  return (
+    <li
+      className='group bg-white w-80 h-80 p-4 rounded-md 
+          hover:bg-orange-500 hover:text-white transition-colors duration-300 ease-in-out'
+    >
+      <div className='flex flex-col h-full w-full'>
+        <h1 className='text-black font-bold group-hover:text-white '>
+          {consultantName}
+        </h1>
+        <span className='text-gray-500 font-light group-hover:text-white'>
+          {consultantCities}
+        </span>
+        <hr />
+        <section className='text-gray-500 mt-6 group-hover:text-white'>
+          {consultantArea}
+        </section>
+
+        <ConsultantRate />
+
+        <button className='w-full border p-2 font-bold mt-auto rounded-sm'>
+          Fazer Proposta
+        </button>
+      </div>
+    </li>
+  );
+}
