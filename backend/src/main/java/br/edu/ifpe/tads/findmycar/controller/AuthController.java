6fe43bb2f43ef5419c9f52bd29e5500612fe5ffa
Luiz Marcel <64820964+lmarcel@users.noreply.github.com>
2024-01-14 17:38:50 -0300

    feature done - foto usuário

--- backend/src/main/java/br/edu/ifpe/tads/findmycar/controller/AuthController.java
+++ backend/src/main/java/br/edu/ifpe/tads/findmycar/controller/AuthController.java
@@ -1,6 +1,5 @@
 package br.edu.ifpe.tads.findmycar.controller;
 
-
 import br.edu.ifpe.tads.findmycar.controller.exceptions.BadRequestException;
 import br.edu.ifpe.tads.findmycar.dto.UsuarioDto;
 import br.edu.ifpe.tads.findmycar.infra.security.AuthenticationSucessfull;
@@ -11,73 +10,61 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.validation.Valid;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.User;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
-
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.StandardCopyOption;
-import java.util.Base64;
-import java.util.HashMap;
-import java.util.Map;
 
 @CrossOrigin("http://127.0.0.1:5173")
 @RestController
 @RequestMapping("/api/auth")
 public class AuthController {
 
-    private final AuthenticationManager authenticationManager;
-    private final JWTUtil jwtUtil;
+  private final AuthenticationManager authenticationManager;
+  private final JWTUtil jwtUtil;
 
-    private final PasswordEncoder encoder;
+  private final UsuarioService usuarioService;
 
-    private final UsuarioService usuarioService;
+  public AuthController(AuthenticationManager authenticationManager, JWTUtil jwtUtil, UsuarioService usuarioService) {
+    this.authenticationManager = authenticationManager;
+    this.jwtUtil = jwtUtil;
+    this.usuarioService = usuarioService;
+  }
 
-    public AuthController(AuthenticationManager authenticationManager, JWTUtil jwtUtil, PasswordEncoder encoder, UsuarioService usuarioService) {
-        this.authenticationManager = authenticationManager;
-        this.jwtUtil = jwtUtil;
-        this.encoder = encoder;
-        this.usuarioService = usuarioService;
+  @PostMapping("/login")
+  public ResponseEntity<AuthenticationSucessfull> login(@RequestBody CredencialsDTO credencialsDTO) {
+    Authentication authenticationRequests = UsernamePasswordAuthenticationToken
+        .unauthenticated(credencialsDTO.getEmail(), credencialsDTO.getPassword());
+    Authentication authenticationResponse = this.authenticationManager.authenticate(authenticationRequests);
+
+    User usuario = (User) authenticationResponse.getPrincipal();
+    String token = jwtUtil.generateToken(usuario.getUsername());
+
+    AuthenticationSucessfull authenticationSucessfull = new AuthenticationSucessfull(token);
+
+    return ResponseEntity.ok(authenticationSucessfull);
+  }
+
+  @PostMapping("/criar")
+  public ResponseEntity<?> registrarUsuario(@Valid @RequestParam("pessoaJson") String pessoaJson,
+      @RequestParam("file") MultipartFile file) {
+    ObjectMapper mapper = new ObjectMapper();
+
+    try {
+      UsuarioDto pessoa = mapper.readValue(pessoaJson, UsuarioDto.class);
+      this.usuarioService.criarUsuario(pessoa, file);
+    } catch (BadRequestException e) {
+      return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+    } catch (JsonMappingException e) {
+      throw new RuntimeException("Não foi possível ler o json");
+    } catch (JsonProcessingException e) {
+      throw new RuntimeException("Não foi possível ler o json");
     }
 
-    @PostMapping("/login")
-    public ResponseEntity<AuthenticationSucessfull> login(@RequestBody CredencialsDTO credencialsDTO) {
-        Authentication authenticationRequests = UsernamePasswordAuthenticationToken
-            .unauthenticated(credencialsDTO.getEmail(), credencialsDTO.getPassword());
-        Authentication authenticationResponse = this.authenticationManager.authenticate(authenticationRequests);
-
-        User usuario = (User) authenticationResponse.getPrincipal();
-        String token = jwtUtil.generateToken(usuario.getUsername());
-
-        AuthenticationSucessfull authenticationSucessfull = new AuthenticationSucessfull(token);
-
-        return ResponseEntity.ok(authenticationSucessfull);
-    }
-
-    @PostMapping("/criar")
-    public ResponseEntity registrarUsuario(@Valid @RequestParam("pessoaJson") String pessoaJson , @RequestParam("file") MultipartFile file){
-        ObjectMapper mapper = new ObjectMapper();
-
-        try {
-            UsuarioDto pessoa  = mapper.readValue(pessoaJson, UsuarioDto.class);
-            this.usuarioService.criarUsuario(pessoa, file);
-        } catch (BadRequestException e) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
-        } catch (JsonMappingException e) {
-            throw new RuntimeException("Não foi possível ler o json");
-        } catch (JsonProcessingException e) {
-            throw new RuntimeException("Não foi possível ler o json");
-        }
-
-        return ResponseEntity.status(HttpStatus.CREATED).build();
-    }
+    return ResponseEntity.status(HttpStatus.CREATED).build();
+  }
 }
--- backend/src/main/java/br/edu/ifpe/tads/findmycar/service/impl/UsuarioServiceImpl.java
+++ backend/src/main/java/br/edu/ifpe/tads/findmycar/service/impl/UsuarioServiceImpl.java
@@ -1,5 +1,17 @@
 package br.edu.ifpe.tads.findmycar.service.impl;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
 import br.edu.ifpe.tads.findmycar.controller.exceptions.BadRequestException;
 import br.edu.ifpe.tads.findmycar.dto.UsuarioDTOInfo;
 import br.edu.ifpe.tads.findmycar.dto.UsuarioDto;
@@ -12,181 +24,162 @@ import br.edu.ifpe.tads.findmycar.repository.ClienteRepository;
 import br.edu.ifpe.tads.findmycar.repository.ConsultorRepository;
 import br.edu.ifpe.tads.findmycar.repository.UsuarioRepository;
 import br.edu.ifpe.tads.findmycar.service.UsuarioService;
-import org.apache.tomcat.util.codec.binary.Base64;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
-
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.StandardCopyOption;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Optional;
 
 @Service
 public class UsuarioServiceImpl implements UsuarioService {
-    private static final Logger logger = LoggerFactory.getLogger(UsuarioServiceImpl.class);
-    private final ConsultorRepository consultorRepository;
-    private final ClienteRepository clienteRepository;
-    private final PasswordEncoder passwordEncoder;
-    private final UsuarioRepository usuarioRepository;
-    private final JWTUtil jwtUtil;
+  private final ConsultorRepository consultorRepository;
+  private final ClienteRepository clienteRepository;
+  private final PasswordEncoder passwordEncoder;
+  private final UsuarioRepository usuarioRepository;
+  private final JWTUtil jwtUtil;
 
-    @Value("${file.upload-dir}")
-    private String uploadDir;
+  @Value("${file.upload-dir}")
+  private String uploadDir;
 
-    public UsuarioServiceImpl(ConsultorRepository consultorRepository, ClienteRepository clienteRepository, PasswordEncoder passwordEncoder, UsuarioRepository usuarioRepository, JWTUtil jwtUtil) {
-        this.consultorRepository = consultorRepository;
-        this.clienteRepository = clienteRepository;
-        this.passwordEncoder = passwordEncoder;
-        this.usuarioRepository = usuarioRepository;
-        this.jwtUtil = jwtUtil;
+  public UsuarioServiceImpl(ConsultorRepository consultorRepository, ClienteRepository clienteRepository,
+      PasswordEncoder passwordEncoder, UsuarioRepository usuarioRepository, JWTUtil jwtUtil) {
+    this.consultorRepository = consultorRepository;
+    this.clienteRepository = clienteRepository;
+    this.passwordEncoder = passwordEncoder;
+    this.usuarioRepository = usuarioRepository;
+    this.jwtUtil = jwtUtil;
+  }
+
+  @Override
+  public Optional<Usuario> findUsuarioByEmail(String email) {
+    return this.usuarioRepository.findByEmail(email);
+  }
+
+  @Override
+  public Optional<UsuarioDTOInfo> findUsuarioByJWT(String jwt) {
+    UsuarioDTOInfo usuarioDTOInfo;
+    String email = jwtUtil.getEmailFromToken(jwt);
+
+    Optional<Usuario> usuario = this.usuarioRepository.findByEmail(email);
+    if (usuario.isEmpty())
+      return Optional.empty();
+
+    Optional<Consultor> consultorOptional = this.consultorRepository.findById(usuario.get().getId());
+    Optional<Cliente> clienteOptional = this.clienteRepository.findById(usuario.get().getId());
+
+    if (consultorOptional.isPresent()) {
+      Consultor consultor = consultorOptional.get();
+      usuarioDTOInfo = new UsuarioDTOInfo(
+          consultor.getId(),
+          consultor.getNome(),
+          consultor.getEmail(),
+          consultor.getPrecoDoServico(),
+          consultor.getAreaDeAtuacao(),
+          "CONSULTOR",
+          this.recuperarArquivo(consultor.getFotoPerfil())
+      );
+
+      return Optional.of(usuarioDTOInfo);
     }
 
-    @Override
-    public Optional<Usuario> findUsuarioByEmail(String email) {
-        return this.usuarioRepository.findByEmail(email);
+    if (clienteOptional.isPresent()) {
+      Cliente cliente = clienteOptional.get();
+      usuarioDTOInfo = new UsuarioDTOInfo(
+          cliente.getId(),
+          cliente.getNome(),
+          cliente.getEmail(),
+          "CLIENTE",
+          this.recuperarArquivo(cliente.getFotoPerfil())
+      );
+
+      return Optional.of(usuarioDTOInfo);
     }
 
-    @Override
-    public Optional<UsuarioDTOInfo> findUsuarioByJWT(String jwt) {
-        UsuarioDTOInfo usuarioDTOInfo;
-        String email = jwtUtil.getEmailFromToken(jwt);
+    return Optional.empty();
+  }
 
-        Optional<Usuario> usuario = this.usuarioRepository.findByEmail(email);
-        if (usuario.isEmpty())
-            return Optional.empty();
+  @Override
+  public void criarUsuario(UsuarioDto dto, MultipartFile file) throws BadRequestException {
+    String fileLocation = salvarArquivo(file);
+    System.out.println(fileLocation);
 
-        Optional<Consultor> consultorOptional = this.consultorRepository.findById(usuario.get().getId());
-        Optional<Cliente> clienteOptional = this.clienteRepository.findById(usuario.get().getId());
+    if (TipoUsuario.CONSULTOR.equals(dto.getTipo())) {
+      this.consultorRepository.save(criarConsultor(dto, fileLocation));
+    } else if (TipoUsuario.CLIENTE.equals(dto.getTipo())) {
+      this.clienteRepository.save(criarCliente(dto, fileLocation));
+    } else {
+      throw new BadRequestException("Tipo de usuário não pode ser aceito");
+    }
+  }
 
-        if (consultorOptional.isPresent()) {
-            Consultor consultor = consultorOptional.get();
-            usuarioDTOInfo = new UsuarioDTOInfo(
-                consultor.getId(),
-                consultor.getNome(),
-                consultor.getEmail(),
-                consultor.getPrecoDoServico(),
-                consultor.getAreaDeAtuacao(),
-                "CONSULTOR"
-            );
+  private String getFileExtension(String fileName) {
+    String[] extension = fileName.split("/.");
 
-            return Optional.of(usuarioDTOInfo);
-        }
+    return extension[0];
+  }
 
-        if (clienteOptional.isPresent()) {
-            Cliente cliente = clienteOptional.get();
-            usuarioDTOInfo = new UsuarioDTOInfo(
-                cliente.getId(),
-                cliente.getNome(),
-                cliente.getEmail(),
-                "CLIENTE"
-            );
+  private String salvarArquivo(MultipartFile file) {
+    try {
+      long time = System.currentTimeMillis();
+      String fileName = String.format("%s%s", time, getFileExtension(file.getOriginalFilename()));
+      Path filePath = Path.of(uploadDir, fileName);
+      Files.copy(file.getInputStream(), filePath, StandardCopyOption.REPLACE_EXISTING);
+      return time + file.getOriginalFilename();
+    } catch (IOException e) {
+      return "";
+    }
+  }
 
-            return Optional.of(usuarioDTOInfo);
-        }
+  private String recuperarArquivo(String fileName) {
+    try {
+      Path filePath = Path.of(uploadDir, fileName);
+      byte[] fileContent = Files.readAllBytes(filePath);
+      return java.util.Base64.getEncoder().encodeToString(fileContent);
+    } catch (Exception e) {
+      System.out.printf("Erro ao buscar o arquivo de nome [%s]", fileName);
+      return "";
+    }
+  }
 
-        return Optional.empty();
+  @Override
+  public void atualizarUsuario(UsuarioDto dto, String tokenJWT) {
+    String email = this.jwtUtil.getEmailFromToken(tokenJWT);
+    Optional<Usuario> usuario = this.usuarioRepository.findByEmail(email);
+
+    if (usuario.isEmpty())
+      throw new UsernameNotFoundException("User not found");
+
+    if (dto.getTipo().getName().equalsIgnoreCase("CONSULTOR")) {
+      Consultor consultor = criarConsultor(dto, null);
+      consultor.setId(usuario.get().getId());
+      this.consultorRepository.save(consultor);
     }
 
-    @Override
-    public void criarUsuario(UsuarioDto dto, MultipartFile file) throws BadRequestException {
-        String fileLocation = salvarArquivo(file);
-        if (TipoUsuario.CONSULTOR.equals(dto.getTipo())){
-            this.consultorRepository.save(criarConsultor(dto, fileLocation));
-        }
-        else if(TipoUsuario.CLIENTE.equals(dto.getTipo())){
-            this.clienteRepository.save(criarCliente(dto, fileLocation));
-        }
-        else {
-            throw new BadRequestException("Tipo de usuário não pode ser aceito");
-        }
+    if (dto.getTipo().getName().equalsIgnoreCase("CLIENTE")) {
+      Cliente cliente = criarCliente(dto, null);
+      cliente.setId(usuario.get().getId());
+      this.clienteRepository.save(cliente);
     }
+  }
 
-    private String salvarArquivo(MultipartFile file) {
-        try {
-            Path filePath = Path.of(uploadDir, file.getOriginalFilename());
-            Files.copy(file.getInputStream(), filePath, StandardCopyOption.REPLACE_EXISTING);
+  private Consultor criarConsultor(UsuarioDto dto, String file) {
+    Consultor consultor = new Consultor();
+    consultor.setEmail(dto.getEmail());
+    consultor.setNome(dto.getNome());
+    consultor.setSenha(passwordEncoder.encode(dto.getSenha()));
+    // consultor.setDisponibilidade(dto.getDisponibilidade());
+    // consultor.setAreaDeAtuacao(dto.getAreaDeAtuacao());
+    // consultor.setPrecoDoServico(dto.getPrecoDoServico());
+    consultor.setAreasConsultor(dto.getAreasConsultor());
+    consultor.setAreasBuscador(dto.getAreasBuscador());
+    consultor.setLocais(dto.getLocais());
+    consultor.setFotoPerfil(file);
 
-            byte[] fileContent = Files.readAllBytes(filePath);
-            return java.util.Base64.getEncoder().encodeToString(fileContent);
-        }catch (IOException e) {
-            return "";
-        }
-    }
+    return consultor;
+  }
 
-    private String convertFileToBase64(MultipartFile file) {
-        String fileBase64 = "";
-        if (file == null) {
-            return "";
-        }
+  private Cliente criarCliente(UsuarioDto dto, String file) {
+    Cliente cliente = new Cliente();
+    cliente.setEmail(dto.getEmail());
+    cliente.setNome(dto.getNome());
+    cliente.setSenha(passwordEncoder.encode(dto.getSenha()));
+    cliente.setFotoPerfil(file);
 
-        if (file.isEmpty()) {
-            return "";
-        }
-
-        try{
-            byte[] image = Base64.encodeBase64(file.getBytes(), false);
-            fileBase64 = new String(image);
-        } catch(Exception e) {
-            logger.error(e.getMessage());
-        }
-
-        return fileBase64;
-    }
-
-    @Override
-    public void atualizarUsuario(UsuarioDto dto, String tokenJWT) {
-        String email = this.jwtUtil.getEmailFromToken(tokenJWT);
-        Optional<Usuario> usuario = this.usuarioRepository.findByEmail(email);
-
-        if (usuario.isEmpty())
-            throw new UsernameNotFoundException("User not found");
-
-        if (dto.getTipo().getName().equalsIgnoreCase("CONSULTOR")) {
-            Consultor consultor = criarConsultor(dto, null);
-            consultor.setId(usuario.get().getId());
-            this.consultorRepository.save(consultor);
-        }
-
-        if (dto.getTipo().getName().equalsIgnoreCase("CLIENTE")) {
-            Cliente cliente = criarCliente(dto, null);
-            cliente.setId(usuario.get().getId());
-            this.clienteRepository.save(cliente);
-        }
-    }
-
-    private Consultor criarConsultor(UsuarioDto dto, String file){
-        Consultor consultor = new Consultor();
-        consultor.setEmail(dto.getEmail());
-        consultor.setNome(dto.getNome());
-        consultor.setSenha(passwordEncoder.encode(dto.getSenha()));
-        //consultor.setDisponibilidade(dto.getDisponibilidade());
-        //consultor.setAreaDeAtuacao(dto.getAreaDeAtuacao());
-        //consultor.setPrecoDoServico(dto.getPrecoDoServico());
-        consultor.setAreasConsultor(dto.getAreasConsultor());
-        consultor.setAreasBuscador(dto.getAreasBuscador());
-        consultor.setLocais(dto.getLocais());
-        consultor.setFotoPerfil(file);
-
-        return consultor;
-    }
-
-    private Cliente criarCliente(UsuarioDto dto, String file){
-        Cliente cliente = new Cliente();
-        cliente.setEmail(dto.getEmail());
-        cliente.setNome(dto.getNome());
-        cliente.setSenha(passwordEncoder.encode(dto.getSenha()));
-        cliente.setFotoPerfil(file);
-
-        return cliente;
-    }
+    return cliente;
+  }
 }
