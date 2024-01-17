9d16aa600a7ade8ce818bc54b9d0ebefb39c55aa
Luiz Marcel <64820964+lmarcel@users.noreply.github.com>
2024-01-16 22:07:50 -0300

    Propostacontroller

---
+++ backend/src/main/java/br/edu/ifpe/tads/findmycar/controller/PropostaController.java
@@ -0,0 +1,13 @@
+package br.edu.ifpe.tads.findmycar.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin("*")
+@RestController
+@RequestMapping("/api/proposta")
+public class PropostaController {
+
+
+}
