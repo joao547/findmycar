838e70078e66f7a89579b3ed3c06fbf5d97fea88
acferlucas <acferlucas.dev@gmail.com>
2024-01-18 01:28:26 -0300

    create consultant home

--- frontend/src/pages/Proposta/components/ListaProposta.tsx
+++ frontend/src/pages/Proposta/components/ListaProposta.tsx
@@ -8,45 +8,58 @@ type ListaPropostaProps = {
 };
 
 export function ListaProposta({ propostas }: ListaPropostaProps) {
+  const { tipo } = JSON.parse(localStorage.getItem('@user') as string);
+
+  if (propostas.length > 0) {
+    return (
+      <div className='w-full max-w-md p-4 bg-white border border-gray-200 rounded-lg shadow sm:p-8 dark:bg-gray-800 dark:border-gray-700'>
+        <div className='flex items-center justify-between mb-4'>
+          <h5 className='text-xl font-bold leading-none text-gray-900 dark:text-white'>
+            Ultimas Propostas
+          </h5>
+        </div>
+        <div className='flow-root'>
+          <ul className='divide-y divide-gray-200 dark:divide-gray-700'>
+            {propostas?.map((proposta) => (
+              <>
+                <li className='py-3 sm:py-4' key={proposta.idProposta}>
+                  <div className='flex items-center'>
+                    <div className='flex-shrink-0'>
+                      <User className='w-8 h-8 rounded-full' />
+                    </div>
+                    <div className='flex-1 min-w-0 ms-4'>
+                      <p className='text-sm font-medium text-gray-900 truncate dark:text-white'>
+                        {tipo === 'CLIENTE'
+                          ? proposta.nomeConsultor
+                          : proposta.nomeCliente}
+                      </p>
+                      <p className='text-sm text-gray-500 truncate dark:text-gray-400'>
+                        {proposta.tipoServico}
+                      </p>
+                      <p className='text-sm text-gray-500 truncate dark:text-gray-400'>
+                        {proposta.servicoContratado}
+                      </p>
+                      <p className='text-sm text-yellow-500 truncate dark:text-gray-400'>
+                        {proposta.statusAtual}
+                      </p>
+                    </div>
+                    <div className='inline-flex items-center text-base font-semibold text-gray-900 dark:text-white'>
+                      ${proposta.valorAcordado}
+                    </div>
+                  </div>
+                </li>
+              </>
+            ))}
+          </ul>
+        </div>
+      </div>
+    );
+  }
   return (
     <div className='w-full max-w-md p-4 bg-white border border-gray-200 rounded-lg shadow sm:p-8 dark:bg-gray-800 dark:border-gray-700'>
-      <div className='flex items-center justify-between mb-4'>
-        <h5 className='text-xl font-bold leading-none text-gray-900 dark:text-white'>
-          Ultimas Propostas
-        </h5>
-      </div>
-      <div className='flow-root'>
-        <ul className='divide-y divide-gray-200 dark:divide-gray-700'>
-          {propostas?.map((proposta) => (
-            <>
-              <li className='py-3 sm:py-4' key={proposta.idProposta}>
-                <div className='flex items-center'>
-                  <div className='flex-shrink-0'>
-                    <User className='w-8 h-8 rounded-full' />
-                  </div>
-                  <div className='flex-1 min-w-0 ms-4'>
-                    <p className='text-sm font-medium text-gray-900 truncate dark:text-white'>
-                      {proposta.nomeConsultor}
-                    </p>
-                    <p className='text-sm text-gray-500 truncate dark:text-gray-400'>
-                      {proposta.tipoServico}
-                    </p>
-                    <p className='text-sm text-gray-500 truncate dark:text-gray-400'>
-                      {proposta.servicoContratado}
-                    </p>
-                    <p className='text-sm text-yellow-500 truncate dark:text-gray-400'>
-                      {proposta.statusAtual}
-                    </p>
-                  </div>
-                  <div className='inline-flex items-center text-base font-semibold text-gray-900 dark:text-white'>
-                    ${proposta.valorAcordado}
-                  </div>
-                </div>
-              </li>
-            </>
-          ))}
-        </ul>
-      </div>
+      <h5 className='text-xl font-bold leading-none text-gray-900 dark:text-white'>
+        nenhuma proposta encontrada
+      </h5>
     </div>
   );
 }
--- frontend/src/pages/home/components/consultantHome/consultantHome.tsx
+++ frontend/src/pages/home/components/consultantHome/consultantHome.tsx
@@ -1,33 +1,78 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
+import Select from 'react-select';
 import { api } from '../../../../service/api';
+import { ClientUser } from '../ConsultantProfile';
+import { PropostaCliente } from '../../../Proposta/Proposta';
+import {
+  GeneralOption,
+  statesOptions,
+} from '../../../Login/data/signupMultiStepSelectOptions';
+import { ListaProposta } from '../../../Proposta/components/ListaProposta';
+
+const statusOptions = [
+  { label: 'Todos', value: 'ALL' },
+  { label: 'Aceito', value: 'ACEITO' },
+  { label: 'Não Aceito', value: 'NAO_ACEITO' },
+  { label: 'Esperando', value: 'ESPERANDO' },
+  { label: 'Finalizado', value: 'FINALIZADO' },
+];
 
 export function ConsultantHome() {
-  const [consultants, setConsultants] = useState<any>();
-  useEffect(() => {
+  const [propostas, setPropostas] = useState<Array<PropostaCliente>>([]);
+  const propostaStatusSelect = useRef<any>(null);
+
+  async function handlerSearch() {
     const token = localStorage.getItem('@token');
 
-    async function fetchData() {
-      const { data } = await api.get('/api/consultor/todos', {
-        headers: {
-          Authorization: `Bearer ${token}`,
-        },
-      });
+    const response = await api.get<ClientUser>('/api/user/me', {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
 
-      setConsultants(data);
-    }
+    const { id } = response.data;
+    const status = propostaStatusSelect.current
+      .getValue()
+      .map((opt: GeneralOption) => opt.value)
+      .join(',');
 
-    fetchData();
-  }, []);
+    const { data } = await api.get('/api/proposta/propostasConsultor', {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+      params: {
+        idDono: id,
+        statusBuscado: status,
+      },
+    });
+
+    setPropostas(data);
+  }
+
   return (
     <div>
-      <ol>
-        {consultants?.map((c: any) => (
-          <li key={c.id} className='border p-4 shadow-md bg-slate-50'>
-            {JSON.stringify(c)}
-          </li>
-        ))}
-      </ol>
+      <section className='flex items-center justify-center'>
+        <div className='bg-white h-20 w-full rounded-md flex items-center p-4 gap-4'>
+          <div className='w-full'>
+            <Select
+              options={statusOptions}
+              placeholder='Status da Proposta'
+              ref={propostaStatusSelect}
+            />
+          </div>
+
+          <button
+            className='bg-orange-500 h-full w-40 rounded-md text-white'
+            onClick={handlerSearch}
+          >
+            Buscar
+          </button>
+        </div>
+      </section>
+      <div className='mt-4'>
+        <ListaProposta propostas={propostas} />
+      </div>
     </div>
   );
 }
